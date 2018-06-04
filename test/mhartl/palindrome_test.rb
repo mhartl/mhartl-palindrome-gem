19bb93f7f7356e1af9b39c529ab4ad8a3bdf5b15
Michael Hartl <michael@michaelhartl.com>
2018-06-04 08:11:51 -0700

    Fix reporter version number

--- test/mhartl/palindrome_test.rb
+++ test/mhartl/palindrome_test.rb
@@ -1,16 +1,11 @@
 require "test_helper"
 
-
 class Mhartl::PalindromeTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Mhartl::Palindrome::VERSION
   end
 
-  def test_pending
-    skip "will do"
-  end
-
-  def test_respond_to_palindrome
-    assert "".respond_to?(:length)
+  def test_it_does_something_useful
+    assert false
   end
 end
